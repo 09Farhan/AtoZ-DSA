6baaa36f914833c51056f40cb87da24d0041dc1a
Farhan Ansari <117701058+09Farhan@users.noreply.github.com>
2024-08-11 02:19:59 +0530

    Update patterns.cpp

--- AtoZ DSA/patterns/patterns.cpp
+++ AtoZ DSA/patterns/patterns.cpp
@@ -269,10 +269,33 @@ void print19(int n){
         cout<<endl;
     }
 }
+void print20(int n){
+    int space = 2*n-2;
+    for(int i =1; i<=2*n-1; i++){
+        int stars = i;
+        if(i>n) stars = 2*n-i;
+         //for stars
+        for(int j=1; j<=stars; j++){
+            cout<<"*";
+        }
+        //for space
+        for (int j =1; j<=space; j++){
+            cout<<" ";
+        }
+        //for stars
+        for(int j=1; j<=stars; j++){
+            cout<<"*";
+        
+        }
+        cout<<endl;
+        if(i<n) space-=2;
+        else space+=2;
+    }
+}
 int main(){
     int n;
     cout<<"Enter the number of rows: ";
     cin>>n;
-    print19(n);
+    print20(n);
     return 0;
 }
