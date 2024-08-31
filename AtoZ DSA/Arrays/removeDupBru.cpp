bc5aa08dc7505deb17c12643fc36c58e1fcb193b
Farhan Ansari <117701058+09Farhan@users.noreply.github.com>
2024-09-01 03:36:38 +0530

    remove duplicates brute force added
    remove the duplicates from the array using set and brute force approach code added in c++

---
+++ AtoZ DSA/Arrays/removeDupBru.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int removeDup(int arr[], int n){
+    set<int> st;
+    for(int i =0; i<n; i++){
+        st.insert(arr[i]);
+    }
+    int index=0;
+    for(auto it : st){
+        arr[index] = it;
+        index++;
+    }
+    return index;
+}
+
+int main(){
+    int n;
+    cout<<"Enter the number of elements in the array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements of the array: ";
+    for(int i=0; i<n;i++){
+        cin>>arr[i];
+    }
+    int remove = removeDup(arr,n);
+    cout<<"Array after removing duplicates: ";
+    for(int i = 0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
