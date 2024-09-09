5ceb9a52c28536422325fd2ada16afd3f85b9b2a
Farhan Ansari <117701058+09Farhan@users.noreply.github.com>
2024-09-09 19:04:09 +0530

    updated the problem statement
    updated the problem statement as it is only for positive integers

--- AtoZ DSA/Arrays/longestSubArr.cpp
+++ AtoZ DSA/Arrays/longestSubArr.cpp
@@ -1,4 +1,4 @@
-//Given an array and a sum k, we need to print the length of the longest subarray that sums to k
+//Given an array and a sum k, we need to print the length of the longest subarray that sums to k(positives only)
 #include<bits/stdc++.h>
 using namespace std;
 
