951805047f260fff7e13a39e3c10d3c64bce4d43
Farhan Ansari <117701058+09Farhan@users.noreply.github.com>
2024-09-13 05:27:29 +0530

    added the function sortArray
    added the function of sortArray which does the sorting of the array 0s 1s and 2s

--- AtoZ DSA/Arrays/sortArr.cpp
+++ AtoZ DSA/Arrays/sortArr.cpp
@@ -3,7 +3,19 @@
 using namespace std;
 
 void sortArray(vector<int> &arr, int n){
-
+    int low = 0, mid =0, high = n-1;
+    while(mid<=high){
+        if(arr[mid] == 0){
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }else if(arr[mid] == 1){
+            mid++;
+        }else{
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
 }
 
 int main(){
@@ -19,7 +31,7 @@ int main(){
     sortArray(arr,n);
     cout<<"After sorting the array: "<<endl;
     for(int i = 0; i<n; i++){
-        cout<<arr[i];
+        cout<<arr[i]<<" ";
     }
     cout<<endl;
     return 0;
