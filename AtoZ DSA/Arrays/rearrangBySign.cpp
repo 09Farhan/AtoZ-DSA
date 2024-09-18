39e6195d90c6ed66a1b31044c9a89f410872a4b7
Farhan Ansari <117701058+09Farhan@users.noreply.github.com>
2024-09-18 11:46:37 +0530

    added the rearrange by sign algo
    added the code for the problem statement: There’s an array ‘A’ of size ‘N’ with an equal number of positive and negative elements. Without altering the relative order of positive and negative elements, you must return an array of alternately positive and negative values.
    
    in c++ using the vector stl container

---
+++ AtoZ DSA/Arrays/rearrangBySign.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> rearrangeBySign(vector<int> &arr, int n ){
+    vector<int> ans(n,0);
+
+    int posIndex = 0, negIndex =1;
+
+    for(int i = 0; i<n; i++){
+        if(arr[i]<0){
+            ans[negIndex] = arr[i];
+            negIndex +=2;
+        }else{
+            ans[posIndex] = arr[i];
+            posIndex +=2;
+        }
+    }
+    return ans;
+}
+int main(){
+    int n;
+    cout<<"Enter the number of elements in the array: ";
+    cin>>n;
+
+    vector<int> arr(n);
+    cout<<"Enter the elements of the array: ";
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    vector<int> ans = rearrangeBySign(arr, n);
+    
+    for(int i = 0; i<n; i++){
+        cout<<ans[i]<<" ";
+    }
+    return 0;
+}
