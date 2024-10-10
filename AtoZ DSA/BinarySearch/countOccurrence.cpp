fa1eee071f733ac1f042354c4305f2d094d3da77
Farhan Ansari <117701058+09Farhan@users.noreply.github.com>
2024-10-10 21:47:55 +0530

    added the complete code for the countOccurrence.cpp
    added the complete code with functions for the countOccurrence.cpp file

--- AtoZ DSA/BinarySearch/countOccurrence.cpp
+++ AtoZ DSA/BinarySearch/countOccurrence.cpp
@@ -4,10 +4,63 @@
  #include<bits/stdc++.h>
  using namespace std;
  
- int numOfOccur(vector<int> &arr, int n, int x){
+int firstOccurrence(vector<int> &arr, int n, int k) {
+    int low = 0, high = n - 1;
+    int first = -1;
 
- }
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] == k) {
+            first = mid;
+            high = mid - 1;
+        }
+        else if (arr[mid] < k) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return first;
+}
 
+int lastOccurrence(vector<int> &arr, int n, int k) {
+    int low = 0, high = n - 1;
+    int last = -1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] == k) {
+            last = mid;
+            low = mid + 1;
+        }
+        else if (arr[mid] < k) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return last;
+}
+
+
+pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k) {
+    int first = firstOccurrence(arr, n, k);
+    if (first == -1) {
+        return { -1, -1};
+    }
+    int last = lastOccurrence(arr, n, k);
+    return {first, last};
+}
+
+int count(vector<int> &arr, int n, int x){
+    pair<int, int> ans = firstAndLastPosition(arr, n, x);
+    if (ans.first == -1) {
+        return 0;
+    }
+    return (ans.second - ans.first + 1);
+}
  int main(){
     int n;
     cout<<"Enter the number of elements in the array: ";
@@ -23,7 +76,7 @@
     cout<<"Enter the target element to search the occurences for: ";
     cin>>x;
 
-    int ans = numOfOccur(arr,n,x);
+    int ans = count(arr,n,x);
 
     cout<<"The number of times "<<x<<" occurs is: "<<ans<<endl;
 
