94f254138a2b7461ff939baa39084371f3645465
Farhan Ansari <117701058+09Farhan@users.noreply.github.com>
2024-10-13 21:06:22 +0530

    added the function findMin in the code
    and the program is complete here which is to find the minimum in the rotated array and the problem statement is:-  Given an integer array arr of size N, sorted in ascending order (with distinct values). Now the array is rotated between 1 to N times which is unknown. Find the minimum element in the array.

--- AtoZ DSA/BinarySearch/minRotatedArr.cpp
+++ AtoZ DSA/BinarySearch/minRotatedArr.cpp
@@ -6,8 +6,25 @@ Find the minimum element in the array. */
 using namespace std;
 
 int findMin(vector<int> &arr, int n){
-
+    int low = 0, high = n-1;
+    int ans = INT_MAX;
+    while(low <= high){
+        int mid = (low + high)/2;
+        if(arr[low] <= arr[high]){
+            ans = min(ans, arr[low]);
+            break;
+        }
+        if(arr[low] <= arr[mid]){
+            ans = min(ans, arr[low]);
+            low = mid + 1;
+        }else{
+            ans = min(ans, arr[mid]);
+            high = mid - 1;
+        }
+    }
+    return ans;
 }
+
 
 int main(){
     int n;
