1ad7cddd840d54fe2ce2d435b91518645d0bbd9c
Farhan Ansari <117701058+09Farhan@users.noreply.github.com>
2024-10-21 14:52:21 +0530

    added the problem statement
    The problem statement is: Given an integer array arr of size N, sorted in ascending order (with distinct values).
    Now the array is rotated between 1 to N times which is unknown. 
    Find how many times the array has been rotated.

--- AtoZ DSA/BinarySearch/numOfRotation.cpp
+++ AtoZ DSA/BinarySearch/numOfRotation.cpp
@@ -1,3 +1,7 @@
+/*Given an integer array arr of size N, sorted in ascending order (with distinct values).
+Now the array is rotated between 1 to N times which is unknown. 
+Find how many times the array has been rotated. */
+
 #include<bits/stdc++.h>
 using namespace std;
 
