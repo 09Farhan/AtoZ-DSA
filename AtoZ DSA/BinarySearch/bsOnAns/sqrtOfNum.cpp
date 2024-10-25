c3f51f0c6589f80123799342df15747d01312ac5
Farhan Ansari <117701058+09Farhan@users.noreply.github.com>
2024-10-25 12:42:27 +0530

    added Binary search on answers folder in BS
    added a new folder under Binary Search which is lec 2 of step 4 on striver's a to z dsa course also added the skull of the code for finding the square root of the given number in c++

---
+++ AtoZ DSA/BinarySearch/bsOnAns/sqrtOfNum.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findSqrt(int n){
+
+}
+
+int main(){
+    int n;
+    cout<<"Enter the number you want to find the square root of: ";
+    cin>>n;
+
+    int ans = findSqrt(n);
+
+    cout<<"The square root of the number is: "<<ans<<endl;
+
+    return 0;
+}
