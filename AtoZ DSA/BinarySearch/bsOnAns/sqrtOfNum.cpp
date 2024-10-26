4046fc595a6385017ef52d14b896f5ba04263224
Farhan Ansari <117701058+09Farhan@users.noreply.github.com>
2024-10-26 19:38:45 +0530

    added the function findSqrt
    added the function findSqrt which will find the square root of the number in c++

--- AtoZ DSA/BinarySearch/bsOnAns/sqrtOfNum.cpp
+++ AtoZ DSA/BinarySearch/bsOnAns/sqrtOfNum.cpp
@@ -2,7 +2,18 @@
 using namespace std;
 
 int findSqrt(int n){
+    int low = 1, high = n;
+    while (low <= high){
+        long long mid = (low + high) / 2;
+        long long val = mid * mid;
 
+        if(val <= (long long)(n)){
+            low = mid + 1;
+        }else{
+            high = mid - 1;
+        }
+    }
+    return high;
 }
 
 int main(){
