02a86acb256bf5b8149e4621d47b5d93f51e4552
Farhan Ansari <117701058+09Farhan@users.noreply.github.com>
2024-10-27 23:29:14 +0530

    added the problem statement
    added the exact problem statement in the code as a comment

--- AtoZ DSA/BinarySearch/bsOnAns/sqrtOfNum.cpp
+++ AtoZ DSA/BinarySearch/bsOnAns/sqrtOfNum.cpp
@@ -1,3 +1,5 @@
+/*You are given a positive integer n. Your task is to find and return its square root. If ‘n’ is not a perfect square, then return the floor value of 'sqrt(n)'.*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
