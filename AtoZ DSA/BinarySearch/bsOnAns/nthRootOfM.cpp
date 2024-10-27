5bda7e5279ba80a5221c9a2efca6fcb62533b9de
Farhan Ansari <117701058+09Farhan@users.noreply.github.com>
2024-10-27 23:39:32 +0530

    added the nthRootofM code skull
    added the skull for finding the nth root of M number in c++

---
+++ AtoZ DSA/BinarySearch/bsOnAns/nthRootOfM.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int func(int mid, int n, int m){
+
+}
+
+int nthRoot(int n, int m){
+
+}
+
+int main(){
+    int n;
+    cout<<"Enter the root number: ";
+    cin>>n;
+
+    int m;
+    cout<<"Enter the number you want to find the root of n: ";
+    cin>>m;
+
+    int ans = nthRoot(n, m);
+
+    cout<<"The "<<n<<"th root of"<<m<<" is: "<<ans<<endl;
+
+    return 0;
+
+}
