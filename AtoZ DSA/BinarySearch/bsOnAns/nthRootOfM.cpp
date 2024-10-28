03164f6773410c1d07e90dc86ab0c53618d4527b
Farhan Ansari <117701058+09Farhan@users.noreply.github.com>
2024-10-28 15:12:29 +0530

    completed the fuctions for finding the root
    completed both the function for finding the nthRoot of M and added the problem statement that is:- Given two numbers N and M, find the Nth root of M. 
    The nth root of a number M is defined as a number X when raised to the power N equals M. 
    If the 'nth root is not an integer, return -1
    
    in c++
    now the code for finding the nth root of m is completed

--- AtoZ DSA/BinarySearch/bsOnAns/nthRootOfM.cpp
+++ AtoZ DSA/BinarySearch/bsOnAns/nthRootOfM.cpp
@@ -1,12 +1,39 @@
+/*Given two numbers N and M, find the Nth root of M. 
+The nth root of a number M is defined as a number X when raised to the power N equals M. 
+If the 'nth root is not an integer, return -1.*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
 int func(int mid, int n, int m){
-
+    long long ans = 1;
+    for(int i = 0; i<n; i++){
+        ans = ans * mid;
+        if(ans > m){
+            return 2;
+        }
+    }
+    if(ans == m){
+        return 1;
+    }
+    return 0;
 }
 
 int nthRoot(int n, int m){
+    int low = 1, high = m;
 
+    while(low <= high){
+        int mid = (low + high)/2;
+        int midN = func(mid, n, m);
+        if(midN == 1){
+            return mid;
+        }else if(midN == 0){
+            low = mid + 1;
+        }else {
+            high = mid - 1;
+        }
+    }
+    return -1;
 }
 
 int main(){
