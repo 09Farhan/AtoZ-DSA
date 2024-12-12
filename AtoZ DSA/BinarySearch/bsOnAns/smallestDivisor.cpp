bf9169bcdcc092183a56437370c57b5e82383304
Farhan Ansari <117701058+09Farhan@users.noreply.github.com>
2024-12-12 16:47:32 +0530

    completed the final function
    completed the final function for finding the smallest divisor in the given threshold in c++ and with this the code is completed

--- AtoZ DSA/BinarySearch/bsOnAns/smallestDivisor.cpp
+++ AtoZ DSA/BinarySearch/bsOnAns/smallestDivisor.cpp
@@ -11,7 +11,20 @@ int sumByD(vector<int> &arr, int div){
 }
 
 int smallestDivisor(vector<int> &arr, int limit){
-    
+    int n = arr.size();
+    if(n > limit){
+        return -1;
+    }
+    int low = 1, high = *max_element(arr.begin(), arr.end());
+    while(low <= high){
+        int mid = (low + high)/ 2;
+        if(sumByD(arr, limit) <= limit){
+            high = mid - 1;
+        }else{
+            low = mid + 1;
+        }
+    }
+    return low;
 }
 
 int main(){
